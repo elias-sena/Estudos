1a2d0db89643344f5b3e831af3d2bec890c11bea
Elias <elias.sena@bol.com.br>
2022-04-20 09:17:24 -0300

    Add files via upload

---
+++ javaScript/aula 08/ex11.js
@@ -0,0 +1,6 @@
+var vel = 72
+console.log(`A velocidade do sue carro é de ${vel} KM/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade, MULTADO`)
+}
+console.log(`Diriga com sinto segurança`)
